9871f5d7f2364412d08b52023f0a8f88d4df8c7e
Jenny <jenniferonsamer@hotmail.com>
2017-11-21 00:44:19 -0800

    css origanization

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -68,6 +68,7 @@ get_header(); ?>
 <!-- shop -->
 <h1>inhabitent journal</h1>
 <section class="inhab-journal container">
+<div class= "journal-info">
 	<?php
     $args = array( 'numberposts' => '3', 'order' => 'ASC', );
     $product_posts = get_posts( $args );?>
@@ -77,14 +78,15 @@ get_header(); ?>
 						<?php if ( has_post_thumbnail() ) : ?>
 			<?php the_post_thumbnail( 'medium' ); ?>
 		<?php endif; ?>
+  
+		<div class="entry-meta">
+			<?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_by(); ?>
+		</div><!-- .entry-meta -->
 
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
-						<div class="entry-meta">
-			<?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_by(); ?>
-		</div><!-- .entry-meta -->
-		<a href="<?php echo get_the_permalink();?>"> <p>Read More</p></a>
-
+		<a href="<?php echo get_the_permalink();?>"> <p>Read Entry</p></a>
+		</div>
             <?php endforeach; wp_reset_postdata(); ?>
   
 </section><!-- Journal container -->
